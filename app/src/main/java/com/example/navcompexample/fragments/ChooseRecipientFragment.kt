3198a35d4b68100d887c62c90431e03569c1f1ce
sajal0limbu <40119982+sajal0limbu@users.noreply.github.com>
2019-11-01 19:35:54 +0545

    Initial commit

---
+++ app/src/main/java/com/example/navcompexample/fragments/ChooseRecipientFragment.kt
@@ -0,0 +1,44 @@
+package com.example.navcompexample.fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.navcompexample.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ChooseRecipientFragment : Fragment(), View.OnClickListener {
+
+    var navController: NavController? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_choose_recipient, container, false)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.next_btn -> navController!!.navigate(R.id.action_chooseRecipientFragment_to_specifyAmountFragment)
+            R.id.cancel_btn -> activity!!.onBackPressed()
+
+        }
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.next_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
+    }
+}
